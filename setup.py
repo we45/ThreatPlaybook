de669f3d24500ecb326b1651d40d7e7b3ae9396e
Abhay Bhargav <abhay@we45.com>
2018-04-30 09:35:57 +0530

    Changes to setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,33 @@
 from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='ThreatPlaybook',
     version='0.8',
     packages=[''],
     entry_points={
-          'console_scripts': [
-              'threat-playbook = threat_playbook:execute_from_command_line'
-          ]
+        'console_scripts': [
+            'threat-playbook = threat_playbook:execute_from_command_line'
+        ]
     },
     package_dir={'': 'threat_playbook'},
     url='https://we45.gitbook.io/threatplaybook/',
     license='MIT',
     author='we45',
     author_email='info@we45.com',
-    description='Threat-Models-as Code, An Action-Oriented Threat Modeling and Automation Framework'
+    install_requires=[
+        'mongoengine==0.15.0',
+        'pathlib==1.0.1',
+        'PyYAML==3.12',
+        'robotframework==3.0.4'
+    ],
+    description='Threat-Models-as Code, An Action-Oriented Threat Modeling and Automation Framework',
+    long_description = long_description,
+    long_description_content_type='text/markdown'
 )
