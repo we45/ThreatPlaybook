c17e62b831becb52d7b2e6566887cc5ea372954f
Abhay Bhargav <abhay@we45.com>
2018-06-22 17:16:51 +0530

    Multiple changes
    * Added BurpSuite Result support

--- threat_playbook/utils.py
+++ threat_playbook/utils.py
@@ -2,6 +2,13 @@ from models import *
 import json
 from base64 import b64encode
 from schema import Schema, And, Use, Optional
+import lxml.etree as xml
+import os
+import re
+
+script_dir = os.path.dirname(__file__)
+file_path = os.path.join(script_dir, 'burp_db.json')
+cwe_dict = json.load(open(file_path,'r'))
 
 vul_schema = Schema(
     {
@@ -217,3 +224,79 @@ def manage_brakeman_results(json_file, target, session):
 
             vul_dict.session = session
             vul_dict.save()
+
+def manage_burp_xml_file(xml_file, target, session):
+    try:
+        nreport = xml.parse(xml_file)
+    except (xml.XMLSyntaxError, xml.ParserError):
+        raise Exception("Unable to parse XML")
+    root_elem = nreport.getroot()
+    reg_path = r'issue/name'
+    uniq_objs = root_elem.xpath(reg_path)
+    vuls = set([i.text for i in uniq_objs])
+    p = '{0}[text() = $name]'.format(reg_path)
+    severity_dict = {
+        'Information': 0,
+        'Low': 1,
+        'Medium': 2,
+        'High': 3
+    }
+    burp_confidence_dict = {
+        "Certain": 3,
+        "Firm": 2,
+        "Tentative": 1,
+    }
+    for v in vuls:
+        obj = root_elem.xpath(p, name=v)
+        url_param_list = []
+        for u in obj:
+            parent_obj = u.getparent()
+            req = parent_obj.find('requestresponse/request')
+            res = parent_obj.find('requestresponse/response')
+            request = response = b64encode('')
+            if req is not None:
+                is_base64_encoded = True if req.get('base64') == 'true' else False
+                if is_base64_encoded:
+                    request = req.text
+                else:
+                    request = b64encode(req.text)
+            if res is not None:
+                is_base64_encoded = True if res.get('base64') == 'true' else False
+                if is_base64_encoded:
+                    response = res.text
+                else:
+                    response = b64encode(res.text)
+            url = 'http:/%s' % (parent_obj.findtext('path', default=''))
+            url_param_list.append({
+                'url': parent_obj.findtext('location', default=''),
+                # 'name':parent_obj.findtext('path',default=''),
+                'attack': parent_obj.findtext('issueDetailItems/issueDetailItem', default=''),
+                'name': parent_obj.findtext('issueDetailItems/issueDetailItem', default=''),
+                'request': request,
+                'response': response,
+            })
+        vul_name = parent_obj.findtext('name', default='')
+        severity = parent_obj.findtext('severity', '')
+        issue_type = parent_obj.findtext('type', '8389632')
+        if severity:
+            severity = severity_dict.get(severity)
+        cwe_present = cwe_dict.get(issue_type, [])
+        cwe = 0
+        if cwe_present:
+            cwe = cwe_present[0]
+        desc = parent_obj.findtext('issueBackground', default='')
+        solution = parent_obj.findtext('remediationBackground', default='')
+        observation = parent_obj.find('issueDetail')
+        confidence = parent_obj.findtext('confidence', default='')
+        if confidence:
+            confidence = burp_confidence_dict.get(confidence)
+        if observation is not None:
+            s = '''You should manually examine the application behavior and attempt to identify any unusual input validation or other obstacles that may be in place.'''
+            obs = observation.text.replace(s, '')
+        else:
+            obs = ''
+
+        vul = Vulnerability()
+        vul.name = re.sub('<[^<]+?>', '',vul_name)
+        vul.tool = "Burp"
+        vul.severity = severity
