6d50053eb9c6237285f6902aca074ead5cfcedce
Abhay Bhargav <abhay@we45.com>
2021-04-11 17:52:16 +0530

    Initial Strapi commit

---
+++ cms/threatplaybook/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cc811c80dc230c64b08f474eafb6a6fa'),
+    },
+  },
+});
---
+++ cms/threatplaybook/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '174f3504-cb13-402b-b7b0-8ce445465322'
+};
